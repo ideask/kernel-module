fd2521b8a6dcf9412a43a14e6bc8dba0c02fe9ef
9982733@qq.com <9982733@qq.com>
2017-04-05 20:12:11 +0800

    end用strlen读取是个0值，什么问题？

--- read_kernel_message/read.c
+++ read_kernel_message/read.c
@@ -1,19 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/klog.h>
-char buffer[1024]={0};
+char buffer[512]={0};
+char buffer1=NULL;
+char buffer2=NULL;
+/*
+char *pinjie(char *str1,char *str2)//拼接字符串函数
+{
+    char *result = malloc(strlen(str1)+strlen(str2)+1);
+    if(result == NULL)
+       exit(1);
+    strcpy(result, str1);
+    strcat(result, str2);
+    return result;
+}
+
+char *get_string (char *buffer)
+{
+    char* delim="\n";//分隔符字符串
+    char* p=0;//第一次调用strtok
+    char* end=0;
+    int count =1;
+    end = strrchr(buffer,'\n');
+    p=strtok(buffer,delim);
+    while(p!=NULL){//当返回值不为NULL时，继续循环
+	if(strcmp(end,p) == 0)
+	{
+           printf("{%d}:%s\n",count,p);//输出分解的字符串
+	}
+	printf("<END>\n");
+        p=strtok(NULL,delim);//继续调用strtok，分解剩下的字符串
+	count++;
+    }
+ 
+}
+*/
 int main()
 {
     char* delim="\n";//分隔符字符串
     char* p=0;//第一次调用strtok
     char* end=0;
     int count =1;
-    klogctl(2,buffer,1024);
+    klogctl(2,buffer,512);
     end = strrchr(buffer,'\n');
-    printf("weibu:%s\n",end);
+    if(end != NULL)
+      printf("weibu:%s\n",end);
     p=strtok(buffer,delim);
     while(p!=NULL){//当返回值不为NULL时，继续循环
-        printf("{%d}:%s\n",count,p);//输出分解的字符串
+	if(strncmp(end,p,strlen(end)) == 0)
+	{
+           printf("{%d}:%s\n",count,p);//输出分解的字符串
+	   printf("the end length is :%d\n",strlen(end));
+	}
 	printf("<END>\n");
         p=strtok(NULL,delim);//继续调用strtok，分解剩下的字符串
 	count++;
